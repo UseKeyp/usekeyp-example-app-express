960728b50f60042f3ef03bc5f705b5efd605c7d2
pi0neerpat <blockchainbuddha@gmail.com>
2023-02-02 11:43:35 -1000

    Change strategy to passport-oauth2

--- db.js
+++ db.js
@@ -1,33 +1,41 @@
-var sqlite3 = require('sqlite3');
-var mkdirp = require('mkdirp');
+var sqlite3 = require("sqlite3");
+var mkdirp = require("mkdirp");
 
-mkdirp.sync('./var/db');
+mkdirp.sync("./var/db");
 
-var db = new sqlite3.Database('./var/db/todos.db');
+var db = new sqlite3.Database("./var/db/todos.db");
 
-db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS users ( \
+db.serialize(function () {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users ( \
     id INTEGER PRIMARY KEY, \
     username TEXT UNIQUE, \
     hashed_password BLOB, \
     salt BLOB, \
     name TEXT \
-  )");
-  
-  db.run("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+    accessToken TEXT \
+    refreshToken TEXT \
+  )"
+  );
+
+  db.run(
+    "CREATE TABLE IF NOT EXISTS federated_credentials ( \
     id INTEGER PRIMARY KEY, \
     user_id INTEGER NOT NULL, \
     provider TEXT NOT NULL, \
     subject TEXT NOT NULL, \
     UNIQUE (provider, subject) \
-  )");
-  
-  db.run("CREATE TABLE IF NOT EXISTS todos ( \
+  )"
+  );
+
+  db.run(
+    "CREATE TABLE IF NOT EXISTS todos ( \
     id INTEGER PRIMARY KEY, \
     owner_id INTEGER NOT NULL, \
     title TEXT NOT NULL, \
     completed INTEGER \
-  )");
+  )"
+  );
 });
 
 module.exports = db;
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oidc');
-var db = require('../db');
-
+var express = require("express");
+var passport = require("passport");
+var OAuth2Strategy = require("passport-oauth2");
+var db = require("../db");
 
 // Configure the Google strategy for use by Passport.
 //
@@ -11,46 +10,73 @@ var db = require('../db');
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
-passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: '/oauth2/redirect/google',
-  scope: [ 'profile' ]
-}, function verify(issuer, profile, cb) {
-  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    issuer,
-    profile.id
-  ], function(err, row) {
-    if (err) { return cb(err); }
-    if (!row) {
-      db.run('INSERT INTO users (name) VALUES (?)', [
-        profile.displayName
-      ], function(err) {
-        if (err) { return cb(err); }
-        var id = this.lastID;
-        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-          id,
-          issuer,
-          profile.id
-        ], function(err) {
-          if (err) { return cb(err); }
-          var user = {
-            id: id,
-            name: profile.displayName
-          };
-          return cb(null, user);
-        });
-      });
-    } else {
-      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-        if (err) { return cb(err); }
-        if (!row) { return cb(null, false); }
-        return cb(null, row);
-      });
+
+const KEYP_APP_DOMAIN =
+  process.env.KEYP_APP_DOMAIN || "https://app.usekeyp.com";
+const APP_DOMAIN = process.env.APP_DOMAIN || "http://localhost:3000";
+passport.use(
+  new OAuth2Strategy(
+    {
+      authorizationURL: `${KEYP_APP_DOMAIN}/oauth2/auth`,
+      tokenURL: `${KEYP_APP_DOMAIN}/oauth2/auth`,
+      clientID: process.env.KEYP_CLIENT_ID,
+      callbackURL: `${APP_DOMAIN}/auth/example/callback`,
+      scope: ["profile", "email"],
+    },
+    function verify(accessToken, refreshToken, profile, cb) {
+      db.get(
+        "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+        [issuer, profile.id],
+        function (err, row) {
+          if (err) {
+            return cb(err);
+          }
+          if (!row) {
+            db.run(
+              "INSERT INTO users (name) VALUES (?)",
+              [profile.displayName],
+              function (err) {
+                if (err) {
+                  return cb(err);
+                }
+                var id = this.lastID;
+                db.run(
+                  "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+                  [id, issuer, profile.id],
+                  function (err) {
+                    if (err) {
+                      return cb(err);
+                    }
+                    var user = {
+                      id: id,
+                      name: profile.displayName,
+                    };
+                    return cb(null, user);
+                  }
+                );
+              }
+            );
+          } else {
+            db.get(
+              "SELECT * FROM users WHERE id = ?",
+              [row.user_id],
+              function (err, row) {
+                if (err) {
+                  return cb(err);
+                }
+                if (!row) {
+                  return cb(null, false);
+                }
+                return cb(null, row);
+              }
+            );
+          }
+        }
+      );
     }
-  });
-}));
-  
+  )
+);
+
 // Configure Passport authenticated session persistence.
 //
 // In order to restore authentication state across HTTP requests, Passport needs
@@ -60,18 +86,17 @@ passport.use(new GoogleStrategy({
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Facebook profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
     cb(null, { id: user.id, username: user.username, name: user.name });
   });
 });
 
-passport.deserializeUser(function(user, cb) {
-  process.nextTick(function() {
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
     return cb(null, user);
   });
 });
-
 
 var router = express.Router();
 
@@ -83,8 +108,8 @@ var router = express.Router();
  * user to sign in with Google.  When the user clicks this button, a request
  * will be sent to the `GET /login/federated/accounts.google.com` route.
  */
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get("/login", function (req, res, next) {
+  res.render("login");
 });
 
 /* GET /login/federated/accounts.google.com
@@ -99,7 +124,7 @@ router.get('/login', function(req, res, next) {
  * Once Google has completed their interaction with the user, the user will be
  * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
  */
-router.get('/login/federated/google', passport.authenticate('google'));
+router.get("/login/keyp", passport.authenticate("google"));
 
 /*
     This route completes the authentication sequence when Google redirects the
@@ -107,19 +132,24 @@ router.get('/login/federated/google', passport.authenticate('google'));
     automatically created and their Google account is linked.  When an existing
     user returns, they are signed in to their linked account.
 */
-router.get('/oauth2/redirect/google', passport.authenticate('google', {
-  successReturnToOrRedirect: '/',
-  failureRedirect: '/login'
-}));
+router.get(
+  "/oauth2/redirect/keyp",
+  passport.authenticate("google", {
+    successReturnToOrRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 /* POST /logout
  *
  * This route logs the user out.
  */
-router.post('/logout', function(req, res, next) {
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
